018cbf01a80f017f2763040398f3c386496d3236
Bohdan Malets <b.malets@klara.com>
2023-03-13 00:40:01 +0100

    turbo streams security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action -> { sleep 2 }
+  before_action :authenticate_user!, unless: :devise_controller?
+  before_action -> { sleep 1 }
+
+  private
+
+  def current_company
+    @current_company ||= current_user.company if user_signed_in?
+  end
+
+  helper_method :current_company
 end
